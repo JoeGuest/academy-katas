f8a1cd433db9116383595b7e5a1b049b57a28a4b
Joseph Guest <JoeGuest@Josephs-MacBook-Air.local>
2014-10-16 14:47:38 +0100

    Writing a starting test

---
+++ lib/potter_discount.rb
@@ -0,0 +1,5 @@
+class Harry_Potter_Discount
+
+  HARRY_POTTER_SERIES = {}
+
+end
--- spec/potter_discount_spec.rb
+++ spec/potter_discount_spec.rb
@@ -1,6 +1,8 @@
-require "potter_discount"
+require 'potter_discount'
 
 RSpec.describe "Harry Potter discount" do
-  it "should store the books in an array" do
+  it "should store the books in a hash to reference" do
+    hash = Harry_Potter_Discount.new
+    expect( hash.class ).to eq Hash
   end
 end
