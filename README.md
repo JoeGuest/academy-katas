0c67c1bc96149d42a2ec9b238c0425408a1e6296
Joseph Guest <JoeGuest@Josephs-MacBook-Air.local>
2014-10-16 14:06:16 +0100

    Setting up structure to then work from

---
+++ README.md
@@ -0,0 +1,1 @@
+Location for all academy kata related exercises
---
+++ spec/potter_discount_spec.rb
@@ -0,0 +1,6 @@
+require "potter_discount"
+
+RSpec.describe "Harry Potter discount" do
+  it "should store the books in an array" do
+  end
+end
